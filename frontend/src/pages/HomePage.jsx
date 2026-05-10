a4232c26f8fbc0aaa910ad7388b43455883ae0bc
Vicky Yadav <ybicky86@gmail.com>
2026-05-11 00:51:52 +0530

    set for stream done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import toast from 'react-hot-toast'
 
 const HomePage = () => {
     return (
-        <div className='bg-red-200'>
+        <div className='bg-red-500'>
             <button onClick={() => toast("hi there")}>Toast</button>
             <button onClick={() => { throw new Error("My first Sentry error!") }}>Error</button>
         </div>
